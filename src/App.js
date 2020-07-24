a8af6a442b27aa739454472e56e0bec7460e68c4
Jason Aviles <sandman2k18@outlook.com>
2020-07-23 23:01:49 -0400

    updsted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route,withRouter } from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
 import Footer from "./Footer";
 import SecondPage from "./SecondPage";
 import FirstPage from "./FirstPage";
@@ -7,36 +7,38 @@ import axios from "axios";
 
 axios.defaults.headers.common = {
   ...axios.defaults.headers.common,
-  'Access-Control-Allow-Origin': 'https://thequantumfocusingmethod.com',
-  "Content-Type": 'application/json',
- 
+  "Access-Control-Allow-Origin": "https://thequantumfocusingmethod.com",
+  "Content-Type": "application/json",
 };
 
 class App extends Component {
-  state = { 
-    username: "", email: "" 
+  state = {
+    username: "",
+    email: "",
   };
-
 
   render() {
     const onChangePdf = (e) => {
-     this.setState({ [e.target.name]: e.target.value });
+      this.setState({ [e.target.name]: e.target.value });
     };
     console.log(this, "props");
 
     const postFetch = (user) => {
       axios
-        .post("https://quantumsmokingquitmethod.herokuapp.com/api/v1", user,{headers:{
-
-         'Access-Control-Allow-Origin': "*",
-         'Access-Control-Allow-Methods': 'POST',
-         "Access-Control-Allow-Headers": "accept, content-type",
-         "Access-Control-Max-Age": "1728000"
-        
-        }})
+        .post("https://cors-anywhere.herokuapp.com/https://quantumsmokingquitmethod.herokuapp.com/api/v1", user, {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "POST",
+            "Access-Control-Allow-Headers": "accept, content-type",
+            "Access-Control-Max-Age": "1728000",
+            crossorigin: "true",
+          },
+        })
         .then((data) => {
           console.log(data, "here");
-          this.props.history.push(`/submited${this.state.username}/${this.state.email} `);
+          this.props.history.push(
+            `/submited${this.state.username}/${this.state.email} `
+          );
         })
         .catch((err) => console.log(err));
     };
@@ -48,21 +50,35 @@ class App extends Component {
 
     return (
       <div className="container">
-    
+        <Route
+          exact
+          path="/"
+          render={(props) => (
+            <FirstPage
+              username={this.state.username}
+              email={this.state.email}
+              {...props}
+              submitForm={submitForm}
+              postFetch={postFetch}
+              onChangePdf={onChangePdf}
+            />
+          )}
+        />
 
-        <Route 
-        exact
-  path='/'
-  render={(props) => <FirstPage username={this.state.username} email={this.state.email} {...props} submitForm={submitForm} postFetch={postFetch}  onChangePdf={onChangePdf}/>}
-/>
-
-<Route 
-        exact
-        path={`/submited${this.state.username}/${this.state.email} `}
-  render={(props) => <SecondPage username={this.state.username} email={this.state.email} {...props} submitForm={submitForm} postFetch={postFetch}  onChangePdf={onChangePdf}/>}
-/>
-   
-
+        <Route
+          exact
+          path={`/submited${this.state.username}/${this.state.email} `}
+          render={(props) => (
+            <SecondPage
+              username={this.state.username}
+              email={this.state.email}
+              {...props}
+              submitForm={submitForm}
+              postFetch={postFetch}
+              onChangePdf={onChangePdf}
+            />
+          )}
+        />
 
         <form onSubmit={submitForm} className="border">
           {" "}
@@ -74,7 +90,7 @@ class App extends Component {
             className="header__input"
           />
           <input
-          onChange={onChangePdf}
+            onChange={onChangePdf}
             name="email"
             value={this.state.email}
             placeholder="email"
@@ -90,4 +106,4 @@ class App extends Component {
   }
 }
 
-export default withRouter( App);
+export default withRouter(App);
